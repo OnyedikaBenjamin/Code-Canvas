2819f9ab29a82560629ed6bf223c9f6dc68dc010
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-04-08 11:24:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let js = 'amazing'
+    if (js == 'amazing')
+        alert('Java Script is Amazing');
+    var orange = 2;
+    var apple = 9;
+    var thingsBought = orange + apple;
+    console.log('I bought a total of ' + thingsBought + ' items.');
+
+    console.log(70 + 250 + 100);
+
+javaScriptIsFun = true;
+year = 1991;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof year);
+let age = 40;
+age = 70;
+console.log(age);
+
+const daddyAge = 2022 - 1965;
+const myAge = 2022 - 2000;
+
+console.log(daddyAge, myAge);
+
+console.log(myAge**2);  // This simply mean myAge to the power of 2.
+
+name = 'Ben-';
+nickName = 'Billion';
+
+console.log(name + nickName);
+
+console.log(myAge > daddyAge);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
